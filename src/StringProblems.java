69de9c1e4d3f5718795583d5386f121a1aec618e
savicst <savicst@ssfs.org>
2021-11-10 09:55:39 -0500

    started string problems

--- src/StringProblems.java
+++ src/StringProblems.java
@@ -1,19 +1,36 @@
 public class StringProblems {
 
     public static String addHyphens(String s) {
-
-        return "";
+        //Pizza -> P-i-z-z-a
+        String newString = "";
+        for (int i=0; i < s.length(); i++){
+            newString += s.substring(i, i+1) + "-";
+        }
+        return newString.substring(0,newString.length() -1);
     }
 
     public static String everyOtherLetter(String s) {
-
-        return "";
+        String newString = "";
+        for (int i=0; i < s.length(); i+=2){
+            newString += s.substring(i, i+1);
+        }
+        return newString;
     }
 
 
     public static String differentStrings(String s1, String s2) {
 
-        return "";
+        if (s1.compareTo(s2) == 0){
+            return s1 + " and " + s2 + " are the same";
+        }
+        else{
+            //not s1, also this doesn't stop at the diff letter
+            int count = 0;
+            for (int i=0; s1.substring(i, i+1).equals(s2.substring(i, i+1)); i++) {
+                count++;
+            }
+            return s1 + " and " + s2 + " are not the same. They differ at letter number " + (count+1);
+        }
     }
 
     public static boolean isPalindrome(String word) {
