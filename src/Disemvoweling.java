251c49f0844cc4ada7eefe7edc1c5c55972b43c6
savicst <savicst@ssfs.org>
2021-11-16 15:18:16 -0500

    finished disemvoweling

--- src/Disemvoweling.java
+++ src/Disemvoweling.java
@@ -2,22 +2,14 @@ import java.util.Scanner;
 public class Disemvoweling {
     public static String disemDouble(String s){
        String returnStatement = "";
-        for (int i=0; i<s.length()-1; i++){
-            if (s.substring(i, i+1).equals(s.substring(i+1, i+2))) {
-            }
-            else{
-                    returnStatement += s.substring(i, i + 1);
-                }
-            //to not get out of bounds maybe do it with no second number
-        }
-            if (s.substring(s.length()-1,s.length()).equals(s.substring(s.length()))){
-                returnStatement+= s.substring(s.length());
-            }
-            else{
+       int removedCount= 1;
+        for (int i = 0; i < s.length() - 1; i++) {
+               if (s.substring(i, i + 1).equals(s.substring(i + 1, i + 2))) {
+               } else {
+                   returnStatement += s.substring(i, i + 1);
+               }
+           }
                 returnStatement += s.substring(s.length()-1);
-            }
-
-        //returnStatement += s.substring(s.length()-1);
         return returnStatement;
     }
     public static String disemVowel(String s){
@@ -28,10 +20,6 @@ public class Disemvoweling {
                 returnStatement += (s.substring(i, i + 1));
             }
         }
-        if (vowels.indexOf(s.substring(s.length()))==-1){
-            returnStatement += (s.substring(s.length()));
-        }
-       // returnStatement += s.substring(s.length()-1);
         return returnStatement;
     }
     public static void main(String[] args){
@@ -39,15 +27,14 @@ public class Disemvoweling {
         System.out.println("Welcome to the disemvoweling utility.");
         System.out.println("Enter your phrase: ");
         String s = scan.nextLine();
-        String x = disemDouble(s);
-        String y = disemVowel(x);
+        String x = disemVowel(s);
+        String y = disemDouble(x);
         System.out.println("The disemvoweled phrase is: " + y);
         System.out.print("Reduced from " +  s.length() + " to " + y.length() + " characters. ");
         double reduc = 100 * (double)(s.length()-y.length())/ s.length();
         reduc+=.05;
         reduc = (int) (reduc*10);
         reduc/= 10;
-        //need to round
         System.out.println("Reduction rate of " + reduc + "%.");
     }
 }
